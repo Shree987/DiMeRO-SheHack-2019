33ccdb1a1a4e9f1b371b3203eff7bfbf52a2c712
Shreeraksha R Aithal <shreeaithal2000@gmail.com>
2019-11-10 09:03:25 +0530

    User pages

--- dimero/app/controllers/reports_controller.rb
+++ dimero/app/controllers/reports_controller.rb
@@ -69,6 +69,6 @@ class ReportsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def report_params
-      params.require(:report).permit(:date, :pid, :repname, :rep)
+      params.require(:report).permit(:date, :patient_id, :repname, :rep)
     end
 end
